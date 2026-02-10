42bd35afe41653e4ebae53ec4dec335941a24cb8
Gabriel Soares <gabriellnobrega21@gmail.com>
2026-02-10 00:24:26 +0000

    hello

--- tarefa01/Main.java
+++ tarefa01/Main.java
@@ -4,7 +4,7 @@ public class Main{
     public static void main (String[] args){
         ret r = new ret();
         ret r2 = new ret();
-        
+        //hello
         r.altura= 3;
         r.base= 4;
  
